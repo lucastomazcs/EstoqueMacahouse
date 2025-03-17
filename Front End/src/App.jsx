116ace622cb2167ab1a6c25f9658ad213cb3c8b2
PrataRicardo <ricardo.jprataf@gmail.com>
2025-03-17 11:34:43 -0300

    Adicionando Front End

---
+++ Front End/src/App.jsx
@@ -0,0 +1,12 @@
+import LoginForm from "./Components/LoginForm/loginForm"
+import './App.css';
+
+function App(){
+  return (
+  <div className="App-header">
+    <LoginForm />
+  </div>
+)
+}
+
+export default App;
