b2a2f179d28a91eb97a975ea2d737fb1cc4880cf
lucastomazcs <lucastomaz023@gmail.com>
2025-03-18 22:22:00 -0300

    Criação das rotas BackEnd para o front

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -3,6 +3,16 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(auth.router)
+
 @app.get("/")
 def read_root():
     return {"message": "Hello, FastAPI!"}
