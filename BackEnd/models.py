2b8a161fee2a10821bded2efe038a59ea3ebf652
Luiza <luizafrsilva@gmail.com>
2025-03-19 18:08:05 -0300

    Criando BackEnd

--- BackEnd/models.py
+++ BackEnd/models.py
@@ -5,5 +5,9 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
+    name = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
     password = Column(String)
+
+    def __repr__(self):
+        return f"<User(id={self.id}, name={self.name}, email={self.email})>"
