bd99cc784c27d60c18642e7ec64b447d71b74656
PrataRicardo <ricardo.jprataf@gmail.com>
2025-03-24 15:25:48 -0300

    Subindo

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginForm from './Components/Forms/LoginForm';
+// import RegisterForm from './Components/Forms/RegisterForm';
+import HomePage from './Components/HomePage/HomePage';
+import SaleForm from './Components/Forms/SaleForm';
+import Inventory from './Components/HomePage/Inventory';
+import UserList from './components/Register';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App-header">
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          
+          {/* <Route path="/register" element={<RegisterForm />} /> */}
+
+          <Route path="/sale" element={<SaleForm />} />
+
+          <Route path="/home" element={<HomePage />} />
+
+          <Route path="/manage" element={<Inventory />}/>
+        </Routes>
+      </Router>
+      <main>
+      <UserList/>
+      </main>
+    </div>
+  );
+}
+
+
+export default App;
