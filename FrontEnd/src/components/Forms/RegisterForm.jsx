3cd6768be64162de08e2c196e9146792c5ea748d
PrataRicardo <ricardo.jprataf@gmail.com>
2025-03-26 13:54:20 -0300

    restoring Forms

--- FrontEnd/src/components/Forms/RegisterForm.jsx
+++ FrontEnd/src/components/Forms/RegisterForm.jsx
@@ -1,24 +1,19 @@
 import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
-const RegisterForm = ({ addUser }) => {
-  const [formData, setFormData] = useState({
-    name: '',
-    login: '',
-    email: '',
-    password: ''
-  });
-  
+const RegisterForm = ({addUsername}) => {
   const [showPassword, setShowPassword] = useState(false);
-
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
+  const [username, setUsername] = useState('');
+  // const navigate = useNavigate();
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    addUser(formData); 
-    setFormData({ name: '', login: '', email: '', password: '' });
+    if (username) {
+      addUsername(username);
+      setUsername(''); 
+    }
   };
 
   return (
@@ -29,37 +24,27 @@ const RegisterForm = ({ addUser }) => {
           type="text" 
           placeholder="Nome" 
           className="custom-input"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
         />
 
         <input 
           type="text" 
           placeholder="Login" 
           className="custom-input"
-          name="login"
-          value={formData.login}
-          onChange={handleChange}
         />
 
         <input 
           type="text" 
           placeholder="Email" 
           className="custom-input"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
         />
-
+        
         <div className="password-container">
           <input 
             type={showPassword ? "text" : "password"} 
             placeholder="Senha" 
             className="custom-input"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
           />
           <i 
             className={`eye-icon ${showPassword ? 'open' : ''}`} 
@@ -69,18 +54,18 @@ const RegisterForm = ({ addUser }) => {
           </i>
         </div>
 
-        <button type="submit" className="custom-button">
+        <Button variant="primary" type="submit" className="custom-button">
           CADASTRAR
-        </button>
+        </Button>
+
+        <div className="navigation-text">
+          <p>
+            Voltar ao Login
+          </p>
+        </div>
       </form>
-      
-      <div className="navigation-text">
-        <p>
-          Voltar ao <Link to="/">Login</Link>
-        </p>
-      </div>
     </div>
   );
-};
+}
 
 export default RegisterForm;
